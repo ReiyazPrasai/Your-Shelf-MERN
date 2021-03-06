d77ef52c0dc1d596c684828f90db358520812fbb
Reiyaz Prasai <prasai.reiyaz@gmail.com>
2021-03-06 17:04:56 +0545

    only backend

--- frontend/src/components/AuthLayout/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import './authLayout.css'
-const AuthLayout = (props) => {
-  return <div className="auth-layout">{props.children}</div>;
-};
-
-export default AuthLayout;
--- frontend/src/containers/Auth/RegisterContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Register from "../../components/Auth/Register/index";
-
-const RegisterContainer = (props) => {
-  return <Register {...props} />;
-};
-
-export default RegisterContainer;
